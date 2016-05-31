319421f53e08507882e78499f1075809f3a263bd
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2016-05-31 14:51:47 -0600

    add html, css, and javascript files

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
-
+    $.get('https://galvanize-cors-proxy.herokuapp.com/http://api.themoviedb.org/3/search/person?query=Sandra+Bullock&api_key=c2e1a90db2b771c4076bbfc772a8baae').then(function(data) {
+        console.log(data);
+    })
 });
